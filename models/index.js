4eddb4da378142fca330a758e514572aa763e88d
alexander nunez <alexandernunz@outlook.com>
2023-02-22 19:52:41 -0500

    index.js for Models draft

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 //import models
-
+const Animal = require('./animal');
+const Cat = require('./cat');
+const Dog = require('./dog');
+const User = require('./user');
 //create relationships
 
+//Animal.belongsToMany(User, {
+    //foreignKey:"user_id",
+//})
+
+//Cat.belongsTo()
 //export modules
