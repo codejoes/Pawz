4114631c97ee845ec513b357d047a47cfd2261a7
alexander nunez <alexandernunz@outlook.com>
2023-03-01 17:55:43 -0500

    Animal routes and formatted relationships model and added animal relationships

---
+++ controller/api/animalRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { Animal } = require('../../models');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newAnimal = await Animal.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newAnimal);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const animalData = await Animal.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!animalData) {
+        res.status(404).json({ message: 'No animal found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(animalData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
