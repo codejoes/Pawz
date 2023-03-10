6c9524ed9b5730e5e296dae1c1c42bd6d4dc7dd4
codejoes <josephscodes@gmail.com>
2023-03-10 00:32:03 -0500

    big css update, remove comments, fix some routes

--- controller/api/commentRoutes.js
+++ controller/api/commentRoutes.js
@@ -1,40 +1,66 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const router = require('express').Router();
+// const { Comment } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id,
-      post_id: 2,
-    });
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const postId = window.location.toString().split('/')[
+//       window.location.toString().split('/').length - 1
+//     ];
 
-    res.status(200).json(newComment);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+//     console.log(postId);
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-        post_id: req.session.post_id,
-      },
-    });
+//     const newComment = await Comment.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//       post_id: postId,
+//     });
 
-    if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
-      return;
-    }
+//     console.log(newComment);
 
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(newComment);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
-module.exports = router;
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const getComments = await Comment.findAll();
+//     res.status(200).json(getComments);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/:id', withAuth, async (req, res) => {
+//   try {
+//     const getComments = await Comment.findByPk(req.params.id);
+//     res.status(200).json(getComments);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const commentData = await Comment.destroy({
+//       where: {
+//         id: req.params.id,
+//         // user_id: req.session.user_id,
+//         // post_id: req.session.post_id,
+//       },
+//     });
+
+//     if (!commentData) {
+//       res.status(404).json({ message: 'No comment found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(commentData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -20,7 +20,7 @@ const newFormHandler = async (event) => {
     } 
 };
 
-  const delButtonHandler = async (event) => {
+const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
@@ -34,12 +34,46 @@ const newFormHandler = async (event) => {
         alert('Failed to delete post');
       }
     }
-  };
+};
   
-  document
+
+const animalHandler = async (event) => {
+  event.preventDefault();
+
+  // const id = window.location.toString().split('/')[
+  //   window.location.toString().split('/').length - 1
+  // ];
+
+  // console.log(id);
+
+  const animalName = document.querySelector('#animal-name').value.trim();
+  const animalType = document.querySelector('#animal-type').value.trim();
+  const animalText = document.querySelector('#animal-text').value.trim();
+  // const post_id = id;
+
+  const response = await fetch (`/api/animals/`, {
+    method: 'POST',
+    body: JSON.stringify( { animalName, animalType, animalText } ),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+ 
+  if (response.ok) {
+    location.reload();
+  } else {
+    alert('Failed to add animal');
+  }
+};
+
+document
     .querySelector('.new-project-form')
     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler); 
+document
+    .querySelector('.post-list')
+    .addEventListener('click', (event) => { delButtonHandler(event) }); 
+
+document
+  .querySelector('#addAnimalBtn')
+  .addEventListener('click', (event) => { animalHandler(event) });
